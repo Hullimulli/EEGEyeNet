63dae9e73b828d15b304fe2af613e71208190a8a
kjoel <kjoel@student.ethz.ch>
2022-07-11 19:29:57 +0200

    Ninas Model

--- DL_Models/torch_models/torch_utils/dataloader.py
+++ DL_Models/torch_models/torch_utils/dataloader.py
@@ -4,14 +4,17 @@ from torch.utils.data import DataLoader, TensorDataset
 from config import config 
 import logging 
 
-def create_dataloader(X, y, batch_size, model_name, shuffle=True, drop_last=True):
+def create_dataloader(X, y, batch_size, model_name, shuffle=True, drop_last=True,isClassification=False):
     """
     Input: X, y of type np.array
     Return: pytorch dataloader containing the dataset of X and y that returns batches of size batch_size 
     """
     # Transform np.array to torch flaot tensor
-    tensor_x = torch.as_tensor(X).float() 
-    tensor_y = torch.as_tensor(y).float()
+    tensor_x = torch.as_tensor(X).float()
+    if isClassification:
+        tensor_y = torch.as_tensor(y).long()
+    else:
+        tensor_y = torch.as_tensor(y).float()
     # Unsqueeze channel direction for eegNet model
     if model_name == 'EEGNet':
         logging.info(f"Unsqueeze data for eegnet")
