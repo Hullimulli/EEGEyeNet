b0289365adc79aa5186d1924b5c088ee20f9ab07
kjoel <kjoel@student.ethz.ch>
2022-08-15 13:53:07 +0200

    Seeds

--- experiments.py
+++ experiments.py
@@ -7,6 +7,6 @@ parser.add_argument('--seed', type=int, default=0, help='an integer for the accu
 
 args = parser.parse_args()
 
-task = method(name='CNN', seed=args.seed, convDimension=1, directory='./MultiDNet', batchSize=32, wandbProject='eegeye')
+task = method(name='CNN', seed=args.seed, convDimension=0, directory='./MultiDNet', batchSize=32, wandbProject='eegeye')
 #task = method(name='CNN_1D',directory='/Users/Hullimulli/Documents/ETH/SA2/localRuns', seed=args.seed,nrOfEpochs=1, convDimension=1,wandbProject='',batchSize=32)
 task.fit(nrOfEpochs=50, saveBool=False)
