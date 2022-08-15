43761f318eadd30f8e5802b71ad90499b2941447
kjoel <kjoel@student.ethz.ch>
2022-08-15 14:23:00 +0200

    Seeds

--- experiments.py
+++ experiments.py
@@ -3,10 +3,12 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Process Experiment Parameters')
-parser.add_argument('--seed', type=int, default=0, help='an integer for the accumulator')
+parser.add_argument('--seed', type=int, default=0, help='seed of the experiment')
+parser.add_argument('--convDimension', type=int, default=1, help='type of convolution, shapes input features')
+parser.add_argument('--name', type=str, default='CNN', help='architecture type')
 
 args = parser.parse_args()
 
-task = method(name='CNN', seed=args.seed, convDimension=0, directory='./MultiDNet', batchSize=32, wandbProject='eegeye')
+task = method(name=args.name, seed=args.seed, convDimension=args.convDimension, directory='./MultiDNet', batchSize=32, wandbProject='eegeye')
 #task = method(name='CNN_1D',directory='/Users/Hullimulli/Documents/ETH/SA2/localRuns', seed=args.seed,nrOfEpochs=1, convDimension=1,wandbProject='',batchSize=32)
 task.fit(nrOfEpochs=50, saveBool=False)
