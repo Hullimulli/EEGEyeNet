d7f8cbe5828202f8a260c14cd3910bcf693d2e91
kjoel <kjoel@student.ethz.ch>
2022-09-14 15:31:11 +0200

    BrainDecode

--- experiments.py
+++ experiments.py
@@ -6,9 +6,9 @@ parser = argparse.ArgumentParser(description='Process Experiment Parameters')
 parser.add_argument('--seed', type=int, default=0, help='seed of the experiment')
 parser.add_argument('--convDimension', type=int, default=1, help='type of convolution, shapes input features')
 parser.add_argument('--name', type=str, default='CNN', help='architecture type')
-
+parser.add_argument('--task', type=str, default='angle', help='task type')
 args = parser.parse_args()
 
-task = method(task='angle', directory='./MultiDNet', batchSize=32, wandbProject='eegeye',name=args.name, seed=args.seed, convDimension=args.convDimension)
-#task = method(name='CNN_1D',directory='/Users/Hullimulli/Documents/ETH/SA2/localRuns', seed=args.seed, convDimension=12, wandbProject='',batchSize=32, task='angle')
-task.fit(nrOfEpochs=25, saveBool=False)
+task = method(task=args.task, directory='./MultiDNet', batchSize=32, wandbProject='eegeye',name=args.name, seed=args.seed, convDimension=args.convDimension)
+#task = method(name='Hybrid4',directory='/Users/Hullimulli/Documents/ETH/SA2/localRuns', seed=args.seed, convDimension=2, wandbProject='',batchSize=32, task='angle')
+task.fit(nrOfEpochs=50, saveBool=False)
